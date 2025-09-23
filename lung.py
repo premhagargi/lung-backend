7e10aae6d49b78c524219c788d783b0a95bbfa25
premhagargi <premhagargi@gmail.com>
2025-09-23 12:24:22 +0530

    first

--- lung.py
+++ lung.py
@@ -339,4 +339,5 @@ if __name__ == '__main__':
     
     # Start Flask API
     print("\nStarting Flask API on http://localhost:5000/predict")
-    app.run(debug=False, host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
